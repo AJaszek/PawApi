94b153abb754209befbeb0b7e5305a73b3cdd6fb
Arek <Arek@192.168.0.19>
2021-06-19 10:40:05 +0200

    Added SSL

--- src/main/java/paw/timetable/files/FileStorageService.java
+++ src/main/java/paw/timetable/files/FileStorageService.java
@@ -34,28 +34,26 @@ public class FileStorageService {
             Files.createDirectories(this.fileStorageLocation);
         } catch (Exception ex) {
             
-            //throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
         }
     }
 
     public String storeFile(MultipartFile file) {
-        // Normalize file name
+        
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
         try {
-            // Check if the file's name contains invalid characters
+            
             if(fileName.contains("..")) {
-               // throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+               return null;
             }
 
-            // Copy file to the target location (Replacing existing file with the same name)
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 
             return fileName;
         } catch (IOException ex) {
             return null;
-            //throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+            
         }
     }
 
@@ -66,11 +64,9 @@ public class FileStorageService {
             if(resource.exists()) {
                 return resource;
             } else {
-               // throw new MyFileNotFoundException("File not found " + fileName);
                return null;
             }
         } catch (MalformedURLException ex) {
-           // throw new MyFileNotFoundException("File not found " + fileName, ex);
            return null;
         }
     }
